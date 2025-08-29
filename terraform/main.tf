0de7836f6bd395ee05a048399fc45cf6b665571e
EC2 Default User <ec2-user@ip-172-31-22-74.us-west-2.compute.internal>
2025-08-29 04:04:34 +0000

    Add Git LFS tracking for large files

--- terraform/main.tf
+++ terraform/main.tf
@@ -104,7 +104,7 @@ locals {
 }
 
 resource "aws_instance" "web" {
-  ami                    = data.aws_ami.al2023.id
+  ami                    = data.aws_ami.amazon_linux.id
   instance_type          = var.instance_type
   subnet_id              = element(data.aws_subnets.default.ids, 0)
   vpc_security_group_ids = [aws_security_group.web_sg.id]
